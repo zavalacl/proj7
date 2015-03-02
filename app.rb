8eaaf38ac98ead1e0fbc8638339ab1be99bbb8f6
Claudia Zavala <zavalacl@msu.edu>
2015-03-02 02:56:04 -0500

    initial commit

--- app.rb
+++ app.rb
@@ -4,7 +4,13 @@
 
 		class HolidayApp < Sinatra::Base
 			get '/' do
-					@holidays = HolidApi.get(country:'US', year: Time.now.year, month: Time.now.month)
+					@holidays = HolidApi.get(
+						country:'US',
+						month: params['month'] || Time.now.month,
+						day: params['day'] || Time.now.day,
+						year: params['year'] || Time.now.year
+
+					)
 				erb :index
 			end
 		end
